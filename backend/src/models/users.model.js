3ea19ebef073d156f011f51282e7c8049d955738
subrat palei <subratpaleibabul@gmail.com>
2026-03-24 17:37:15 +0530

    Feat:backend in good shape

--- backend/src/models/users.model.js
+++ backend/src/models/users.model.js
@@ -1,46 +1,62 @@
 import bcrypt from 'bcrypt';
 import mongoose from 'mongoose';
-const userSchema = new mongoose.Schema({
-  userName: {
-    type: String,
-    required: [true, 'Please enter your name'],
-    unique: [true, 'Username already exists'],
-    minLength: [3, 'Username must be at least 3 characters long'],
-     match: [
-        /^[a-zA-Z0-9]+$/,
-        'Username must contain only letters and numbers'
-      ]
-  },
-  email: {
-    type: String,
-    required: [true, 'Please enter your email'],
-    unique: [true, 'Email already exists'],
-    match: [
+import { v4 as uuidv4 } from 'uuid';
+
+const userSchema = new mongoose.Schema(
+  {
+    email: {
+      type: String,
+      required: [true, 'Email is required'],
+      unique: [true, 'Email already exists'],
+      match: [
         // eslint-disable-next-line no-useless-escape
         /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
         'Please fill a valid email address'
       ]
+    },
+    password: {
+      type: String,
+      required: [true, 'Password is required']
+    },
+    username: {
+      type: String,
+      required: [true, 'Username is required'],
+      unique: [true, 'Username already exists'],
+      minLength: [3, 'Username must be at least 3 characters'],
+      match: [
+        /^[a-zA-Z0-9]+$/,
+        'Username must contain only letters and numbers'
+      ]
+    },
+    avatar: {
+      type: String
+    },
+    isVerified: {
+      type: Boolean,
+      default: false
+    },
+    verificationToken: {
+      type: String
+    },
+    verificationTokenExpiry: {
+      type: Date
+    }
   },
-  password: {
-    type: String,
-    required: [true, 'Please enter your password'],
-  },
-  avatar:{
-    type:String,
-  }
-},{timestamps:true});
+  { timestamps: true }
+);
 
-userSchema.pre('save', async function saveUser() {
-  if (this.isModified('password')) {
-    const salt = await bcrypt.genSalt(9);
-    this.password = await bcrypt.hash(this.password, salt);
+userSchema.pre('save', function saveUser(next) {
+  if (this.isNew) {
+    const user = this;
+    const SALT = bcrypt.genSaltSync(9);
+    const hashedPassword = bcrypt.hashSync(user.password, SALT);
+    user.password = hashedPassword;
+    user.avatar = `https://robohash.org/${user.username}`;
+    user.verificationToken = uuidv4().substring(0, 10).toUpperCase();
+    user.verificationTokenExpiry = Date.now() + 3600000; // 1 hour
   }
-
-  if (!this.avatar) {
-    this.avatar = `https://robohash.org/${this.userName}`;
-  }
+  next();
 });
-
 
 const User = mongoose.model('User', userSchema);
 
