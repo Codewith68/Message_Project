3661e518532a1ab99e19f09d2064e0894940bac9
subrat palei <subratpaleibabul@gmail.com>
2026-03-24 18:46:51 +0530

    Feat : Implement basic folder structure for project

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -3,7 +3,7 @@ import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import { defineConfig, globalIgnores } from 'eslint/config'
-
+import simpleImportSort from 'eslint-plugin-simple-import-sort'
 export default defineConfig([
   globalIgnores(['dist']),
   {
@@ -20,10 +20,16 @@ export default defineConfig([
         ecmaVersion: 'latest',
         ecmaFeatures: { jsx: true },
         sourceType: 'module',
+        'simple-import-sort':simpleImportSort
       },
     },
     rules: {
+      'simple-import-sort/imports': 'error',
+      'simple-import-sort/exports': 'error',
+      "semi": ["error", "always"],
+      "quotes": ["error", "double"],
       'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
+      'react-prop-types/prop-types': 'off',
     },
   },
 ])
